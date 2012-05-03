fff85b90d719908e8b23e66d3063e87a129e2f50
James Arthur <thruflo@gmail.com>
2012-05-03 17:33:00 +0100

    release: bump to `1.1.2`.

---
+++ CHANGES.md
@@ -0,0 +1,4 @@
+
+# 0.1.2
+
+Added `basemodel.should_create_all` configuration option.
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def _read(file_name):
 
 setup(
     name = 'pyramid_basemodel',
-    version = '0.1.1',
+    version = '0.1.2',
     description = 'Global base classes for Pyramid SQLAlchemy applications.',
     author = 'James Arthur',
     author_email = 'username: thruflo, domain: gmail.com',
