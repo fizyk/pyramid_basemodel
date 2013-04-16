e16207883c77694e35fddda0b7f7cbbc82b40613
James Arthur <thruflo@gmail.com>
2013-04-16 13:00:36 +0100

    mixin: add `TouchMixin` class.

--- src/pyramid_basemodel/mixin.py
+++ src/pyramid_basemodel/mixin.py
@@ -4,15 +4,20 @@
 
 __all__ = [
     'PolymorphicBaseMixin',
-    'PolymorphicMixin'
+    'PolymorphicMixin',
+    'TouchMixin',
 ]
 
 import logging
 logger = logging.getLogger(__name__)
 
+from datetime import datetime
+
 from sqlalchemy import Column
 from sqlalchemy import Unicode
 from sqlalchemy.ext.declarative import declared_attr
+
+from pyramid_basemodel import save as save_to_db
 
 class PolymorphicBaseMixin(object):
     """Provides a dynamically generated ``__mapper_args__`` property for
@@ -45,5 +50,35 @@ class PolymorphicMixin(object):
         """Set the ``polymorphic_identity`` value to the lower case class name."""
         
         return {'polymorphic_identity': self.__class__.__name__.lower()}
+    
+
+
+class TouchMixin(object):
+    """Provides ``touch`` and ``propagate_touch`` methods."""
+    
+    def propagate_touch(self):
+        """Override to propagate touch events to relations.
+          
+          Note that this event *should not* be  called in response to an
+          SQLAlchemy ORM attribute modified event, as you can't reliably
+          update relations in an attribute event handler.
+        """
+    
+    def touch(self, propagate=True, now=None, save=None):
+        """Update self.modified."""
+        
+        # Compose.
+        if now is None:
+            now = datetime.utcnow
+        if save is None:
+            save = save_to_db
+        
+        # Update self's modified date.
+        self.modified = now()
+        save(self)
+        
+        # Call propagate touch.
+        if propagate:
+            self.propagate_touch()
     
 
